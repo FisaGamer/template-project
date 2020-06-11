95b2f40af0f579c1c201f628e6beaed7e165c664
Franklin Tranié <tranie@MacBook-Pro-de-Franklin.local>
2020-06-11 15:15:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import math as m
+import numpy as np
+import sympy as sp
+import statistics as s
+import sys
+import os
+import argparse
+
+
+def loop(n, f, *kwargs): [f(*kwargs) for i in range(n)]
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
