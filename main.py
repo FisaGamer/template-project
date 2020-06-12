6732ab7c7f5a482569d2ca1fc0e87302989b6f1f
FisaGamer <franklin.tranie@gmail.com>
2020-06-13 00:34:31 +0200

    Maj + comment code

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import statistics as s
 import sys
 import os
 import argparse
-
+import random as r
+import pprint
 
 def loop_list(n: int, f: callable, *args) -> object:
     """
@@ -14,7 +15,7 @@ def loop_list(n: int, f: callable, *args) -> object:
     :param f: callable
     :param kwargs: args
     """
-    [f(*args) for i in range(n)]
+    return [f(*args) for i in range(n)]
 
 
 def loop(n: int, f: callable, *args) -> object:
@@ -31,13 +32,15 @@ def loop(n: int, f: callable, *args) -> object:
 def main():
     pass
 
-
-class CustomString(str):
-    def tolist(self):
-        return [self[i] for i in range(len(self))]
+def add(f, liste):
+    return liste.append(f)
 
 
 if __name__ == '__main__':
-    main()
-    moi = CustomString("coucou")
-    print(moi.tolist())
+    liste = loop_list(100, loop_list, 10, r.randint, 0, 1)
+    pprint.pprint(liste)
+    average = []
+    for i in liste:
+        average.append(s.mean(i))
+    pprint.pprint(s.mean(average))
+    help()
